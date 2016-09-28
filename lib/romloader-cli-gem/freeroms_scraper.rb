790c4e00c6d9d51b6c35974e695c1c12ea477727
jinstrider2000 <efrainperezjr@live.com>
2016-09-28 13:52:08 -0400

    refactored freerom scraper

--- lib/romloader-cli-gem/freeroms_scraper.rb
+++ lib/romloader-cli-gem/freeroms_scraper.rb
@@ -22,20 +22,25 @@ class FreeromsScraper
     game_list = Nokogiri::HTML(open(url)).css("tr[class^=\"game\"] > td[align=\"left\"]")
     [].tap do |rom_list|
       game_list.each do |game_info|
-        rom_list << {}.tap do |game|
-          game_name = game_info.css("span").text
-          game_name.gsub!(/[[:space:]]{2,}/) {|white_spaces| " "}
-          download_link = game_info.css("a").attribute("href").value
-          download_link.gsub!(/[[:space:]]/) {|white_space| CGI::escape(white_space)} unless download_link.ascii_only?
-          direct_download = Nokogiri::HTML(open(download_link))
-          unless direct_download.css("td#rom > script").empty?
-            game_url = /http:\/\/.+(\.zip|\.7z)/.match(direct_download.css("td#rom > script").first.children.first.text)
-            game[:name] = game_name
-            game_url ? game[:download_url] = game_url[0] : game[:download_url] = ""
-            game[:size] = direct_download.css("td#rom + td[colspan=\"2\"]").first.children.first.text.strip
-          end
-        end
+        download_link = game_info.css("a").attribute("href").value
+        download_link.gsub!(/[[:space:]]/) {|white_space| CGI::escape(white_space)} unless download_link.ascii_only?
+        rom_list << rom_details(download_link)
+      end 
+    end
+  end
+
+  def self.rom_details(url)
+    direct_download = Nokogiri::HTML(open(url))
+    {}.tap do |game|
+      unless direct_download.css("td#rom > script").empty?
+        game_name = direct_download.css("tr.gametitle > td[colspan=\"2\"]").text
+        game_name.gsub!(/[[:space:]]{2,}/) {|white_spaces| " "}
+        game_url = /http:\/\/.+(\.zip|\.7z)/.match(direct_download.css("td#rom > script").first.children.first.text)
+        game[:name] = game_name
+        game_url ? game[:download_url] = game_url[0] : game[:download_url] = ""
+        game[:size] = direct_download.css("td#rom + td[colspan=\"2\"]").first.children.first.text.strip
       end
+      binding.pry
     end
   end
 
@@ -52,27 +57,27 @@ class FreeromsScraper
   private_class_method :rom_index_scrape
 end
 
-=begin: 
-        Test: To see if freeroms.com site can be completely scraped and all roms have complete info.
-        Results: Every rom collected, except Neo Geo Pocket game "Faselei! French"
-----------------------------------------------------------------------------------------        
-full_game_list = []
+# Test: To see if freeroms.com site can be completely scraped and all roms have complete info.
+# Results: Every rom collected, except Neo Geo Pocket game "Faselei! French"
+#----------------------------------------------------------------------------------------        
+#full_game_list = []
+#
+#system_list = FreeromsScraper.system_scrape("http://freeroms.com")
+#
+#system_list.each do |game_system|
+#  game_system[:rom_url].each_value { |url| full_game_list << FreeromsScraper.rom_scrape(url) }
+#  puts "#{game_system[:name]} done!"
+#end
+#
+#no_links = full_game_list.flatten.find_all { |game| game[:download_url] == ""  }
+#
+#if no_links.empty?
+#  puts "Every game has a download link!"
+#else
+#   puts "These games don't have download links:"
+#   no_links.each { |game| puts game[:name]  }
+#end 
+#-----------------------------------------------------------------------------------------
 
-system_list = FreeromsScraper.system_scrape("http://freeroms.com")
-
-system_list.each do |game_system|
-  game_system[:rom_url].each_value { |url| full_game_list << FreeromsScraper.rom_scrape(url) }
-  puts "#{game_system[:name]} done!"
-end
-
-no_links = full_game_list.flatten.find_all { |game| game[:download_url] == ""  }
-
-if no_links.empty?
-  puts "Every game has a download link!"
-else
-   puts "These games don't have download links:"
-   no_links.each { |game| puts game[:name]  }
-end 
------------------------------------------------------------------------------------------
-=end
+FreeromsScraper.rom_details("http://www.freeroms.com/dl_roms/rom_download.php?title=ochan_no_oekaki_logic&system=SNES&game_id=28035")
 
